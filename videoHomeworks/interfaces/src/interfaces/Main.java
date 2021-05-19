0a7fb154fd302d82b3200962cadbd0e4f5e57c81
Ozlem <ozlemakgunoglu@gmail.com>
2021-05-19 11:24:46 +0300

    Polymophism with Interfaces

--- videoHomeworks/interfaces/src/interfaces/Main.java
+++ videoHomeworks/interfaces/src/interfaces/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Bir class birdden fazla classý implemente edebilir
-		ICustomerDal ýcCustomerDal=new OracleCustomerDal();
-	}
+		//ICustomerDal ýcCustomerDal=new OracleCustomerDal();
+		CustomerManager customerManager=new CustomerManager(new OracleCustomerDal());
+		
+		customerManager.add();
 
+		
+		
+		
+	}
 }
