a3d94320bb77f65b5f314318274330ab70dfd665
Ozlem <ozlemakgunoglu@gmail.com>
2021-05-19 13:33:53 +0300

    interfaceDemo project added

---
+++ videoHomeworks/interfaceDemo/src/interfaceDemo/Main.java
@@ -0,0 +1,12 @@
+//Bir bankayý düþünelim iki çeþit personel hem iç kaynak hem dýþkaynaktan istihdamvar
+//Yemek ücreti ve maaþ sadece kendi personeli için geçerli 
+//ama bu iki çalýþanda proje yürütüyor. 
+package interfaceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
